a444608d5fee10800802be81cdf2c91b4690189d
Wenjie Deng <dengw@uw.edu>
2022-12-02 10:20:53 -0800

    workable module of V705_restore_suject_functional_AA_alignment.py

--- scripts/V705_restore_suject_functional_AA_alignment.py
+++ scripts/V705_restore_suject_functional_AA_alignment.py
@@ -1,112 +1,152 @@
 #!/usr/bin/python3
 
 ##########################################################################################
-# Program: V705_functional_consensus_AA_alignment_process.py
-# Purpose: In a Gag/Pol/Env directory with AA sequence alignment fasta files:
-# calculate AA consensus via 0 majority and ignoring gaps,
-# align AA consensus sequences via Muscle,
-# profile-profile align reference AA alignment with consensus alignment
-# multiprocessing
+# Program: V705_restore_subject_functional_AA_alignment.py
+# Purpose: From a cross-subjects functional consensus AA alignment:
+# restore each subject's functional AA alignment based on subject consensus and collapsed sequence alignment,
+# uncollapse to subject's uncollapsed functional AA alignment based on name file
 # Author: Wenjie Deng
-# Date: 2021-12-08
+# Date: 2022-11-21
 ##########################################################################################
 
-import sys, re, os
+import sys
+import re
+import os
 import argparse
-import glob
-import calc_AA_consensus
-import muscle_align
-import append_seqs
-from multiprocessing import Pool, Manager
-
-def worker(file, outdir, logdir, nameSeq):
-    fields = file.split("/")
-    filename = fields[-1]
-    logfilename = filename.replace(".fasta", ".log")
-    logfile = logdir + "/" + logfilename
-
-    with open(logfile, "w") as lfp:
-        # calculate functional AA consensus
-        sampleAAwithConsfile = outdir + "/" + filename.replace(".fasta", "_withCons.fasta")
-        print("=== Calculate consensus sequences in " + file + " ===\n")
-        name, seq = calc_AA_consensus.consensus(file, sampleAAwithConsfile)
-        nameSeq[name] = seq
-        lfp.write("=== Calculate consensus sequences in " + file + " ===" + "\n")
-        lfp.write("input: " + file + "\n")
-        lfp.write("output: " + sampleAAwithConsfile + "\n")
-
-        # output success info
-        lfp.write("*** Succeed ***\n")
+import verify_seq_origin
+import uncollapse_seqs
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("refalignfile", help="reference sequence alignment fasta file")
-    parser.add_argument("-d", "--dir", help="directory to hold input sequence fasta file", nargs="?", const=1, type=str, default=".")
-    parser.add_argument("-p", "--processes", help="number of processes for multiprocessing", nargs="?", const=1, type=int,
-                        default="1")
+    parser.add_argument("consensusalignfile", help="cross-subjects consensus sequence alignment fasta file")
+    parser.add_argument("-ad", "--alignmentdir",
+                        help="directory to hold subject consensus and collapsed sequences alignment fasta files",
+                        nargs="?", const=1, type=str)
+    parser.add_argument("-nd", "--namefiledir",
+                        help="directory to hold subject collapsed sequences name files",
+                        nargs="?", const=1, type=str)
+    parser.add_argument("-sd", "--seqdir",
+                        help="directory to hold subject uncollapsed sequence fasta files",
+                        nargs="?", const=1, type=str)
     args = parser.parse_args()
-    reffile = args.refalignfile
-    dir = args.dir
-    proc = args.processes
+    consalignfile = args.consensusalignfile
+    aligndir = args.alignmentdir
+    namedir = args.namefiledir
+    seqdir = args.seqdir
 
-    outdir = dir + "/functional_consensus_AA_alignment_process_outputs"
-    logdir = dir + "/functional_consensus_AA_alignment_process_logs"
-    if os.path.isdir(outdir) is False:
-        os.mkdir(outdir)
-    if os.path.isdir(logdir) is False:
-        os.mkdir(logdir)
-    files = []
-    for file in glob.glob(os.path.join(dir, '*.fasta')):
-        files.append(file)
+    cwd = os.getcwd()
+    collapsedoutdir = cwd + "/restore_subject_functional_collapsed_AA_alignment_outputs"
+    uncollapsedoutdir = cwd + "/restore_subject_functional_uncollapsed_AA_alignment_outputs"
+    if os.path.isdir(collapsedoutdir) is False:
+        os.mkdir(collapsedoutdir)
+    if os.path.isdir(uncollapsedoutdir) is False:
+        os.mkdir(uncollapsedoutdir)
 
-    manager = Manager()
-    consnameSeq = manager.dict()
-    pool = Pool(proc)
-    pool.starmap(worker, [(file, outdir, logdir, consnameSeq) for file in files])
+    consnames, collapsenames = [], []
+    consnameSeq = {}
+    name, refname, refseq = '', '', ''
+    with open(consalignfile, "r") as cfp:
+        for line in cfp:
+            line = line.strip()
+            if line:
+                linematch = re.search(r"^>(\S+)", line)
+                if linematch:
+                    name = linematch.group(1)
+                    if re.search("HXB2", name):
+                        refname = name
+                    else:
+                        consnames.append(name)
+                    consnameSeq[name] = ''
+                else:
+                    consnameSeq[name] += line
+    refseq = consnameSeq[refname].replace("Z", "*")
 
-    pool.close()
-    pool.join()
+    for consname in consnames:
+        print("=== processing "+consname+" ===")
+        sid, alignfile, namefile, seqfile = '', '', '', ''
+        consseq = consnameSeq[consname].replace("Z", "*")
+        namematch = re.search(r"^(.*?)_functional_consensus", consname)
+        if namematch:
+            sid = namematch.group(1)
+        else:
+            sys.exit("name not formatted: "+consname)
 
-    filefields = files[0].split("/")
-    filename = filefields[-1]
-    filenamefields = filename.split("_")
-    id = filenamefields[0] + "_" + filenamefields[3]
-    alllogfile = logdir + "/" + id +"_functional_consensus_AA_alignment_process.log"
-    consdir = outdir + "/functional_consensus"
-    consensusfile = consdir + "/" + id +"_AA_functional_consensus.fasta"
-    print("=== Write consensus sequences into " + consensusfile + " ===\n")
-    if os.path.isdir(consdir) is False:
-        os.mkdir(consdir)
-    with open(consensusfile, "w") as cfp:
-        for name in consnameSeq:
-            conseq = consnameSeq[name].replace("*", "Z")
-            cfp.write(">"+name+"\n"+conseq+"\n")
-    with open(alllogfile, "w") as afp:
-        for file in files:
-            fields = file.split("/")
-            filename = fields[-1]
-            logfilename = filename.replace(".fasta", ".log")
-            logfile = logdir + "/" + logfilename
-            with open(logfile, "r") as lfp:
-                for line in lfp:
-                    afp.write(line)
-                afp.write("\n")
-        afp.write("=== Write consensus sequences into " + consensusfile + " ===\n\n")
+        for filename in os.listdir(aligndir):
+            file = os.path.join(aligndir, filename)
+            if os.path.isfile(file):
+                if re.search(sid, filename):
+                    alignfile = file
+                    break
+        if alignfile == '':
+            sys.exit("No alignment file for sid " + sid)
 
-        # combine reference and consensus sequences
-        print("=== Combine sequences in " + reffile + " and " + consensusfile + " ===\n")
-        consensusreffile = consensusfile.replace(".fasta", "_withRef.fasta")
-        append_seqs.main(reffile, consensusfile, consensusreffile)
-        afp.write("=== Combine sequences in " + reffile + " and " + consensusfile + " ===\n")
-        afp.write("input: " + reffile + ", " + consensusfile + "\n")
-        afp.write("output: " + consensusreffile + "\n\n")
+        print("== processing "+alignfile+" ==")
+        collapsenames = []
+        nameAAs = {}
+        collapsename = ''
+        with open(alignfile, "r") as afp:
+            for line in afp:
+                line = line.strip()
+                if line:
+                    lmatch = re.search(r"^>(\S+)", line)
+                    if lmatch:
+                        collapsename = lmatch.group(1)
+                        if re.search("_functional_consensus", collapsename) is None:
+                            collapsenames.append(collapsename)
+                            nameAAs[collapsename] = []
+                    else:
+                        if re.search("_functional_consensus", collapsename):
+                            consseqnogap = consseq.replace("-", "")
+                            seqnogap = line.replace("-", "")
+                            if consseqnogap != seqnogap:
+                                sys.exit("consensus sequences not match: "+consseqnogap+" vs. "+seqnogap)
+                        else:
+                            nameAAs[collapsename] = list(line)
 
-        # align consensus and reference sequences
-        print("=== Align consensus and reference sequences in "+consensusreffile+" ===\n")
-        consensusrefalignfile = consensusreffile.replace(".fasta", "_align.fasta")
-        muscle_align.main(consensusreffile, consensusrefalignfile)
-        afp.write("=== Align consensus and sequences in "+consensusreffile+" ===\n")
-        afp.write("input: " + consensusreffile + "\n")
-        afp.write("output: " + consensusrefalignfile + "\n\n")
-        
-        
+        outcollapsefile = os.path.join(collapsedoutdir, sid+"_functional_collapsed.fasta")
+        print("= restoring collapsed functional alignment for "+sid+" =")
+        with open(outcollapsefile, "w") as ofp:
+            ofp.write(">"+refname+"\n"+refseq+"\n")
+            collapseseqcount = 0
+            consaas = list(consseq)
+            for collapsename in collapsenames:
+                collapseseqcount += 1
+                idx = -1
+                aas = []
+                for i in range(len(consaas)):
+                    if consaas[i] == '-':
+                        aas.append('-')
+                    else:
+                        idx += 1
+                        aas.append(nameAAs[collapsename][idx])
+                ofp.write(">"+collapsename+"\n")
+                ofp.write(''.join(aas) + "\n")
+            print("restored total "+str(collapseseqcount)+" collapsed sequences")
+
+        for filename in os.listdir(namedir):
+            file = os.path.join(namedir, filename)
+            if os.path.isfile(file):
+                if re.search(sid, filename):
+                    namefile = file
+                    break
+        if namefile == '':
+            sys.exit("No name file for sid " + sid)
+
+        print("== processing " + namefile + " ==")
+        print("= uncollapsing functional sequences for "+sid+" =")
+        outuncollapsefile = os.path.join(uncollapsedoutdir, sid + "_functional_uncollapsed.fasta")
+        uncollapse_seqs.main(outcollapsefile, namefile, outuncollapsefile)
+
+        for filename in os.listdir(seqdir):
+            file = os.path.join(seqdir, filename)
+            if os.path.isfile(file):
+                if re.search(sid, filename):
+                    seqfile = file
+                    break
+        if seqfile == '':
+            sys.exit("No uncollapsed sequence file for sid " + sid)
+
+        print("== processing " + seqfile + " ==")
+        print("= verifying uncollapsed sequences for " + sid + " =")
+        verify_seq_origin.main(outuncollapsefile, seqfile)
+        print()
